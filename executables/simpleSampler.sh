7bd13747df60a462b8c1c1e615c39381562ee7b8
t.pett@tu-braunschweig.de <t.pett@tu-braunschweig.de>
2021-01-14 16:21:52 +0100

    update simpleSampler.sh

--- executables/simpleSampler.sh
+++ executables/simpleSampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # parameter: 1: root path to input dir (models) 2: root path to output dir (samples) 3: run number 4: seed for random CNF
-java -jar ./simpleSampler.jar 'C:\Users\t.pett\Documents\VaMoS_Data\test\models' 'C:\Users\t.pett\Documents\VaMoS_Data\test\samples' '6' '12'
+java -jar ./simpleSampler.jar 'C:\Users\t.pett\Documents\VaMoS_Data\busybox\models' 'C:\Users\t.pett\Documents\VaMoS_Data\busybox\samples' '6' '6'
